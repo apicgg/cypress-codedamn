531a101e009ce7e8785a0ca3e1e54b9eaef48bb0
Anurag Pramanik <p.anurag@outlook.com>
2022-04-26 23:02:50 +0530

    add some test to check pass word reset page

--- cypress/integration/my-tests/hello-world.test.js
+++ cypress/integration/my-tests/hello-world.test.js
@@ -1,10 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Empty Test', () => {
-  it('We have correct page title', () => {
-    cy.viewport(1280, 720)
-    cy.visit('https://codedamn.com/')
-
+  it('The webpage loads', () => {
     // cy.get('[data-testid=homepage-cta]').click()
+    cy.visit('https://codedamn.com/login')
+  })
+
+  it('The sign in page loads', () => {
+    cy.viewport(1280, 720)
+    // cy.get('.m-auto > .font-semibold').should('exist')
+    cy.contains('Forgot your password').click({ force: true })
+    cy.url().should('include', '/password-reset')
+    cy.go('back')
   })
 })
