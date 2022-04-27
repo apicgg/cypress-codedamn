4d2f0f4a22f1a0d433c7dffd81ddbfabad7d834e
Anurag Pramanik <p.anurag@outlook.com>
2022-04-27 08:33:42 +0530

    add some tests

--- cypress/integration/my-tests/hello-world.test.js
+++ cypress/integration/my-tests/hello-world.test.js
@@ -1,15 +1,18 @@
 /// <reference types="cypress" />
 
-describe('Empty Test', () => {
-  it('The webpage loads', () => {
-    // cy.get('[data-testid=homepage-cta]').click()
+describe('Demo Test', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720)
+    console.log(1)
     cy.visit('https://codedamn.com/login')
   })
 
-  it('The sign in page loads', () => {
-    cy.viewport(1280, 720)
-    // cy.get('.m-auto > .font-semibold').should('exist')
+  it('The forget password page loads', () => {
+    console.log(2)
     cy.contains('Forgot your password').click({ force: true })
+    // cy.debug()
+    cy.log(3)
+    console.log(3)
     cy.url().should('include', '/password-reset')
     cy.go('back')
   })
